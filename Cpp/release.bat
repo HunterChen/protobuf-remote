412062d583efaeee409e7e09191c629cf422e8d6
Niall Ryan <Niall Ryan@localhost>
2010-08-11 21:00:47 +1200

    some fixes to release batch

--- Cpp/release.bat
+++ Cpp/release.bat
@@ -2,21 +2,27 @@
 
 if "%1" == "" goto Help
 
-md Release
-md Release\%1
+rmdir /s /q temp_release
+del "%1.zip"
 
-robocopy Source "Release\%1\Source" *.h *.cpp *.cc *.proto *.sln *.vcxproj *.vcxproj.filters /s
-robocopy Lib "Release\%1\Lib" *.lib *.pdb /s
+md temp_release
+md temp_release\%1
 
-pushd Release
+robocopy Source "temp_release\%1\Source" *.h *.cpp *.cc *.proto *.sln *.vcxproj *.vcxproj.filters /s
+robocopy Lib "temp_release\%1\Lib" *.lib *.pdb /s
+
+pushd temp_release
 7z a "%1.zip" "%1\"
 popd
+
+copy "temp_release\%1.zip" .
+rmdir /s /q temp_release
 
 goto End
 
 :Help
 
 echo. Takes one parameter, the name of the release, e.g.
-echo.   release.bat protobuf-remote-1.0
+echo.   release.bat protobuf-remote-cpp-1.0
 
 :End
