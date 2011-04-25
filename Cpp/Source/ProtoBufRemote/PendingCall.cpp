d96dabf15f7dae1d0f8260117f036118c78ee0b4
Niall Ryan <Niall Ryan@localhost>
2011-04-25 21:24:47 +1200

    PendingCall makes a copy of the result message so it will be available on the waiting thread, and added missing synchronization

--- Cpp/Source/ProtoBufRemote/PendingCall.cpp
+++ Cpp/Source/ProtoBufRemote/PendingCall.cpp
@@ -1,18 +1,30 @@
 
 #include "ProtoBufRemote/PendingCall.h"
 
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
 #include "ProtoBufRemote/Parameter.h"
 
 
 namespace ProtoBufRemote {
 
 PendingCall::PendingCall(int id)
-	: m_id(id), m_isCompleted(false), m_result(NULL)
+	: m_id(id), m_isCompleted(false), m_resultMessage(NULL), m_result(NULL)
 {
+    m_event = CreateEvent(NULL, TRUE, FALSE, NULL);
+}
+
+PendingCall::~PendingCall()
+{
+    CloseHandle(m_event);
+    delete m_result;
+    delete m_resultMessage;
 }
 
 void PendingCall::Wait()
 {
+    WaitForSingleObject(m_event, INFINITE);
 }
 
 const Parameter* PendingCall::GetResult()
@@ -23,8 +35,12 @@ const Parameter* PendingCall::GetResult()
 void PendingCall::SetResult(const RpcMessage::Parameter* parameter)
 {
 	if (parameter)
-		m_result = new Parameter(*parameter);
+    {
+        m_resultMessage = new RpcMessage::Parameter(*parameter);
+		m_result = new Parameter(*m_resultMessage);
+    }
 	m_isCompleted = true;
+    SetEvent(m_event);
 }
 
 }
